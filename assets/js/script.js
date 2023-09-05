ae8110e9d0bd085353d91e619a653528d52cf15d
BrunaRihl <brunarihl@gmail.com>
2023-09-05 12:34:45 +0100

    feat: add script and function to build the game board

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+const board = document.getElementById("board"); // Get the game board element.
+const signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "pisces", "scorpio", "capricorn", "aquarius", "sagitarius", "libra"];
+const deck = [...signs, ...signs]; // Create a deck with pairs of zodiac signs.
+
+// Function to build the game board.
+
+function buildBoard(list){
+    for (const index in list) {
+        board.insertAdjacentHTML(
+        "beforeend", 
+        `<div class="card-back" data-name=${list[index]}></div>`
+        )
+    };
+
+    let cards = document.querySelectorAll('.card-back');
+    cards.forEach(card => card.addEventListener('click', flipCard));
+}
+
+/* Function to initialize the game */
+
+function initGame() {
+    board.innerHTML = '';
+    buildBoard(deck);
+ }
+
+ initGame();
+
