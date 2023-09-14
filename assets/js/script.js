81ee6642def089d91ebecc4d8a20681fb270d580
BrunaRihl <brunarihl@gmail.com>
2023-09-14 15:55:15 +0100

    feat: add features section to readme

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const board = document.getElementById("board"); // Get the game board element.
-const signs = ["aries", "taurus"];
+const signs = ["aries", "taurus", "gemini", "cancer", "leo", "virgo", "pisces", "scorpio", "capricorn", "aquarius", "sagitarius", "libra"];
 const deck = [...signs, ...signs]; // Create a deck with pairs of zodiac signs.
 
 let flipped = false;
